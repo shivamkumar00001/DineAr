20af238377d4f3268dd380ad99d0070490a62f9c
shivamkumar00001 <shivamkumar933401@gmail.com>
2025-11-22 23:37:58 +0530

    Initial commit

---
+++ restaurant-side-frontend/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './global.css'
+
+function App() {
+  
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline bg-red-300 text-center">
+    let's do it!
+  </h1>
+    </>
+  )
+}
+
+export default App
